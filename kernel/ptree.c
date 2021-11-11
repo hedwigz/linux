fe650961ddb1652d7d4c22c4019c96b5204afe66
hedwigz <amit@ariga.io>
2021-11-11 17:11:30 +0200

    a

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,28 +1,40 @@
 #include <linux/prinfo.h>
 #include <asm/errno.h>
+#include <linux/kmod.h>
 
-// DEFINE_SPINLOCK(register_lock);
+DEFINE_SPINLOCK(register_lock);
 
 typedef int (*ptree_func)(struct prinfo *buf, int *nr, int pid);
 ptree_func ptree_impl;
 int register_ptree(ptree_func func)
 {
-  // spin_lock(&register_lock);
-	ptree_impl = func;
-  // spin_unlock(&register_lock);
+  spin_lock(&register_lock);
+  if (ptree_impl == NULL) {
+    ptree_impl = func;
+    spin_unlock(&register_lock);
+    return 0;
+  }
+  return -EBUSY;
 }
 
 void unregister_ptree(ptree_func func)
 {
+  spin_lock(&register_lock);
 	ptree_impl = NULL;
+  spin_unlock(&register_lock);
 }
 
 asmlinkage int sys_ptree(struct prinfo *buf, int *nr, int pid)
 {
-	if (ptree_impl != NULL) {
-		return ptree_impl(buf);
-	} else {
-		return -ENOSYS;
-	}
-	return 0;
+  spin_lock(&register_lock);
+  if (ptree_impl == NULL) {
+    request_module("ptree");
+  }
+  if (ptree_impl == NULL) {
+    spin_unlock(&register_lock);
+    return -ENOSYS;
+  }
+  spin_unlock(&register_lock);
+  // TODO: handle memory copy between userspace and kernel space 
+  return ptree_impl(buf, nr, pid);
 }

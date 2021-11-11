13e9df5d4a52bfed04600bc57242e599e87c0385
hedwigz <amit@ariga.io>
2021-11-11 16:57:11 +0200

    fix ptree syscall

--- kernel/ptree.c
+++ kernel/ptree.c
@@ -1,11 +1,15 @@
 #include <linux/prinfo.h>
 #include <asm/errno.h>
 
+// DEFINE_SPINLOCK(register_lock);
+
 typedef int (*ptree_func)(struct prinfo *buf, int *nr, int pid);
 ptree_func ptree_impl;
 int register_ptree(ptree_func func)
 {
+  // spin_lock(&register_lock);
 	ptree_impl = func;
+  // spin_unlock(&register_lock);
 }
 
 void unregister_ptree(ptree_func func)
@@ -16,7 +20,7 @@ void unregister_ptree(ptree_func func)
 asmlinkage int sys_ptree(struct prinfo *buf, int *nr, int pid)
 {
 	if (ptree_impl != NULL) {
-		return module_impl(buf);
+		return ptree_impl(buf);
 	} else {
 		return -ENOSYS;
 	}

2f58547a4994fedf34a9848ce42980981b2cb0d8
hedwigz <amit@ariga.io>
2021-11-24 14:00:35 +0200

    init maps module

---
+++ mmap_module/maps.c
@@ -0,0 +1,41 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/audit.h>
+#include <linux/cred.h>
+#include <linux/sched.h>
+#include <linux/prinfo.h>
+#include <linux/ptree.h>
+#include <linux/slab.h>
+#include <linux/sched/task.h>
+#include <linux/sched/signal.h>
+#include <linux/limits.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Amit Shani");
+MODULE_DESCRIPTION("the extended /proc/self/maps");
+MODULE_VERSION("0.01");
+
+task_struct * t get_self_task_struct() {
+	struct task_struct* ret;
+	pid = task_pid_nr(current);
+	rcu_read_lock();
+	ret = pid_task(find_vpid(pid), PIDTYPE_PID);
+	rcu_read_unlock();
+	return ret;
+}
+
+static int __init maps_init(void)
+{
+	printk(KERN_INFO "Hello, World!\n");
+
+	return 0;
+}
+
+static void __exit maps_exit(void)
+{
+	printk(KERN_INFO "Goodbye, World!\n");
+}
+
+module_init(maps_init);
+module_exit(maps_exit);

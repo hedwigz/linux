0aacf3e38c7811ef8ee7dcf78bea0e827fd67bae
Amit Shani <amit.shani1995@gmail.com>
2021-11-29 21:37:09 +0200

    test program

---
+++ mmap_module/main.c
@@ -0,0 +1,21 @@
+#include <sys/types.h>
+#include <linux/prinfo.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/syscall.h>
+#include <limits.h>
+
+int main(int argc, char *argv[])
+{
+  int size = 8196;
+  char * buf = malloc(size);
+  // sys_mapspages(unsigned long start, unsigned long end, char __user *buf, size_t size)
+  int got = syscall(450, 0, INT_MAX, buf, size);
+  // int got = ptree(ps, &nr, 0);
+  for (i = 0; i< got; i++) {
+    printf("%s\n", buf);
+  }
+}
